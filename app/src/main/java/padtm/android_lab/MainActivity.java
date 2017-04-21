9a25a1440be59a6e377c5d5db58b788cebb48086
Blazej <blazejmoscinski@gmail.com>
2017-04-21 13:20:41 +0200

    Formularz dodawania pliku

--- app/src/main/java/padtm/android_lab/MainActivity.java
+++ app/src/main/java/padtm/android_lab/MainActivity.java
@@ -1,7 +1,11 @@
 package padtm.android_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
                 this.target);
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
+    }
+
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
     }
 }
